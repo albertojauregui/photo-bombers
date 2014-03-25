38e1da9190b068b2046f81518bd13af3917dc368
Alberto Jauregui <bastertwo@gmail.com>
2014-03-24 17:48:26 -0700

    Initial Commit

---
+++ Photo Bombers/AJCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AJCAppDelegate.h
+//  Photo Bombers
+//
+//  Created by Alberto Jauregui on 3/24/14.
+//  Copyright (c) 2014 Alberto Jauregui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AJCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Photo Bombers/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Photo Bombers
+//
+//  Created by Alberto Jauregui on 3/24/14.
+//  Copyright (c) 2014 Alberto Jauregui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AJCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AJCAppDelegate class]));
+    }
+}
